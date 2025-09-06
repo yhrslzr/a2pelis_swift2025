99af7c308c1f3f84f5cb0ae4ef991b67f24ea8b4
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-09-05 18:15:21 -0600

    Initial Commit

---
+++ a2pelis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  a2pelis
+//
+//  Created by WIN603 on 05/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ a2pelis/a2pelisApp.swift
@@ -0,0 +1,17 @@
+//
+//  a2pelisApp.swift
+//  a2pelis
+//
+//  Created by WIN603 on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct a2pelisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
